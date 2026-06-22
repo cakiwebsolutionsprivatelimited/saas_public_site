e1cd4f8c877407a539434fde3b493e1dee8f3696
Loresh CW <loreshkumardash@csplerp.com>
2026-06-22 11:27:14 +0530

    project setup

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-xl">
-      <div className="bg-primary text-inverse rounded-lg p-lg shadow-md">
-        Cakiweb SaaS Project Started
-      </div>
-    </main>
-  );
-}
