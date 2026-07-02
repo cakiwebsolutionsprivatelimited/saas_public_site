b2631d5fcf58921963d5bb3d215f8470afbd7eb1
Loresh CW <loreshkumardash@csplerp.com>
2026-07-02 11:50:18 +0530

    use scrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter } from 'react-router-dom';
+import ScrollToTop from './components/common/ScrollToTop';
 import AppRoutes from './routes';
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <ScrollToTop />
       <AppRoutes />
     </BrowserRouter>
   );
